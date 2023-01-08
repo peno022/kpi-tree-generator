3d26b03698889b200e033d153b4e421769e7aa7a
peno022 <40317050+peno022@users.noreply.github.com>
2023-01-08 16:38:44 +0900

    react-d3-treeを導入

--- app/javascript/components/react_hello.js
+++ app/javascript/components/react_hello.js
@@ -1,13 +1,53 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import Tree from "react-d3-tree";
 
-const Hello = (props) => <div>こんにちは {props.name} さん！</div>;
-
-Hello.defaultProps = {
-  name: "名無し",
+const orgChart = {
+  name: "CEO",
+  children: [
+    {
+      name: "Manager",
+      attributes: {
+        department: "Production",
+      },
+      children: [
+        {
+          name: "Foreman",
+          attributes: {
+            department: "Fabrication",
+          },
+          children: [
+            {
+              name: "Worker",
+            },
+          ],
+        },
+        {
+          name: "Foreman",
+          attributes: {
+            department: "Assembly",
+          },
+          children: [
+            {
+              name: "Worker",
+            },
+          ],
+        },
+      ],
+    },
+  ],
 };
+
+function OrgChartTree() {
+  return (
+    // `<Tree />` will fill width/height of its container; in this case `#treeWrapper`.
+    <div id="treeWrapper" style={{ width: "50em", height: "20em" }}>
+      <Tree data={orgChart} />
+    </div>
+  );
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.getElementById("app");
-  createRoot(container).render(<Hello name="やまうちなお" />);
+  createRoot(container).render(<OrgChartTree />);
 });
