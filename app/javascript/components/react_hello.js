dff822d1a36efe33f5616afb335646af3792a3cf
peno022 <40317050+peno022@users.noreply.github.com>
2023-01-08 13:45:43 +0900

    react-archerのexample2を表示 Please enter the commit message for your changes. Lines starting

--- app/javascript/components/react_hello.js
+++ app/javascript/components/react_hello.js
@@ -1,71 +1,92 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { ArcherContainer, ArcherElement } from "react-archer";
-
-const rootStyle = { display: "flex", justifyContent: "center" };
+const rootStyle = {
+  display: "flex",
+  justifyContent: "center",
+};
 const rowStyle = {
   margin: "200px 0",
   display: "flex",
   justifyContent: "space-between",
 };
-const boxStyle = { padding: "10px", border: "1px solid black" };
+const boxStyle = {
+  padding: "10px",
+  border: "1px solid black",
+};
 
-document.addEventListener("DOMContentLoaded", () => {
-  const container = document.getElementById("app");
-  createRoot(container).render(
-    <div style={{ height: "500px", margin: "50px" }}>
+const SecondExample = () => {
+  const [nbElements, setNbElements] = React.useState(3);
+  const [labels, setLabels] = React.useState("hello");
+  return (
+    <div
+      style={{
+        height: "500px",
+        margin: "50px",
+      }}
+    >
+      <div>
+        <div>Change labels</div>
+        <input
+          data-cy="change-labels-input"
+          type="text"
+          onChange={(event) => {
+            setLabels(event.currentTarget.value);
+          }}
+        />
+      </div>
+      <div>
+        <div>Add elements</div>
+        <button
+          data-cy="add-element"
+          onClick={() => setNbElements(nbElements + 1)}
+        >
+          +
+        </button>
+        <button
+          onClick={() => setNbElements(nbElements > 1 ? nbElements - 1 : 0)}
+        >
+          -
+        </button>
+      </div>
       <ArcherContainer strokeColor="red">
         <div style={rootStyle}>
-          <ArcherElement
-            id="root"
-            relations={[
-              {
-                targetId: "element2",
-                targetAnchor: "top",
-                sourceAnchor: "bottom",
-                style: { strokeDasharray: "5,5" },
-              },
-            ]}
-          >
+          <ArcherElement id="root with spaces et accents héhéhéhé">
             <div style={boxStyle}>Root</div>
           </ArcherElement>
         </div>
 
         <div style={rowStyle}>
-          <ArcherElement
-            id="element2"
-            relations={[
-              {
-                targetId: "element3",
-                targetAnchor: "left",
-                sourceAnchor: "right",
-                style: { strokeColor: "blue", strokeWidth: 1 },
-                label: <div style={{ marginTop: "-20px" }}>Arrow 2</div>,
-              },
-            ]}
-          >
-            <div style={boxStyle}>Element 2</div>
-          </ArcherElement>
-
-          <ArcherElement id="element3">
-            <div style={boxStyle}>Element 3</div>
-          </ArcherElement>
-
-          <ArcherElement
-            id="element4"
-            relations={[
-              {
-                targetId: "root",
-                targetAnchor: "right",
-                sourceAnchor: "left",
-                label: "Arrow 3",
-              },
-            ]}
-          >
-            <div style={boxStyle}>Element 4</div>
-          </ArcherElement>
+          {Array(nbElements)
+            .fill(0)
+            .map((_, i) => (
+              <ArcherElement
+                key={`element${i}`}
+                id={`element${i}`}
+                relations={[
+                  {
+                    targetId: "root with spaces et accents héhéhéhé",
+                    targetAnchor: "bottom",
+                    sourceAnchor: "top",
+                    label: (
+                      <div>
+                        {i} {labels}
+                      </div>
+                    ),
+                  },
+                ]}
+              >
+                <div style={boxStyle}>Element {i}</div>
+              </ArcherElement>
+            ))}
         </div>
       </ArcherContainer>
     </div>
   );
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("app");
+  createRoot(container).render(<SecondExample></SecondExample>);
+  3;
 });
