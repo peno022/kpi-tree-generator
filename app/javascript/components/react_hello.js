b63c818af4eb0bf9184af18aefb0da2f10afb52d
peno022 <40317050+peno022@users.noreply.github.com>
2023-01-09 19:31:35 +0900

    daisyUI導入

--- app/javascript/components/react_hello.js
+++ app/javascript/components/react_hello.js
@@ -141,7 +141,7 @@ const OrgChartTree = () => {
             const newData = JSON.parse(JSON.stringify(data));
             setData(newData);
           }}
-          className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+          className="btn btn-primary"
         >
           <span>追加</span>
         </button>
