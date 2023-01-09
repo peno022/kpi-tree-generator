fb3f983460a51d431df1fb5b4ec11b335619aeed
peno022 <40317050+peno022@users.noreply.github.com>
2023-01-09 19:18:54 +0900

    ボタンにスタイルを適用

--- app/javascript/components/react_hello.js
+++ app/javascript/components/react_hello.js
@@ -130,7 +130,7 @@ const OrgChartTree = () => {
   return (
     <div>
       <div>
-        <div>「訪問者数」の下に階層を追加</div>
+        <div className="underline">「訪問者数」の下に階層を追加</div>
         <button
           onClick={() => {
             console.log("button clicked");
@@ -141,6 +141,7 @@ const OrgChartTree = () => {
             const newData = JSON.parse(JSON.stringify(data));
             setData(newData);
           }}
+          className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
         >
           <span>追加</span>
         </button>
