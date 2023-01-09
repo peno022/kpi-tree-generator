4f647da4655002023d5604fd3f17a68aec79b272
peno022 <40317050+peno022@users.noreply.github.com>
2023-01-09 09:52:13 +0900

    ノード内にノード名を表示する

--- app/javascript/components/react_hello.js
+++ app/javascript/components/react_hello.js
@@ -38,14 +38,63 @@ const orgChart = {
   ],
 };
 
-function OrgChartTree() {
+const customNodeElement = ({ nodeDatum }) => {
+  return (
+    <g>
+      <rect
+        x="-75"
+        rx="5"
+        ry="5"
+        style={{
+          width: "150",
+          height: "85",
+          fill: "ghostwhite",
+          stroke: "dimgray",
+          strokeWidth: "1",
+        }}
+      />
+      <text
+        x="-60"
+        y="38"
+        style={{
+          fill: "#333",
+          strokeWidth: "0",
+          fontWeight: "bold",
+          fontSize: "1.4em",
+          maxWidth: "280",
+        }}
+      >
+        {nodeDatum.name}
+      </text>
+      <text
+        x="-60"
+        y="60"
+        style={{
+          fill: "#333",
+          strokeWidth: "0",
+          fontSize: "0.9em",
+          maxWidth: "280",
+        }}
+      >
+        {`部署：${nodeDatum.attributes?.department}`}
+      </text>
+    </g>
+  );
+};
+
+const OrgChartTree = () => {
   return (
     // `<Tree />` will fill width/height of its container; in this case `#treeWrapper`.
-    <div id="treeWrapper" style={{ width: "50em", height: "20em" }}>
-      <Tree data={orgChart} />
+    <div id="treeWrapper" style={{ width: "50em", height: "50em" }}>
+      <Tree
+        data={orgChart}
+        pathFunc="step"
+        orientation="vertical"
+        renderCustomNodeElement={customNodeElement}
+      />
     </div>
   );
-}
+};
 
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.getElementById("app");
