c1512cea53f35f3d00b0320b111c30a369a21484
peno022 <40317050+peno022@users.noreply.github.com>
2023-01-21 14:24:45 +0900

    rubocop自動修正

--- app/controllers/react_controller.rb
+++ app/controllers/react_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ReactController < ApplicationController
   def hello; end
 end
--- spec/models/sample_spec.rb
+++ spec/models/sample_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 describe 'sample true' do
   it 'returns true' do
     expect(true).to eq(true)
