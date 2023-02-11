3a240b85c65f1b6b154ee55028621b32d7933440
peno022 <40317050+peno022@users.noreply.github.com>
2023-02-11 19:48:55 +0900

    コメント修正（rubocop）

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@
 
 class HomeController < ApplicationController
   def index
-    # TODO:　ログイン機能を実装後
+    # TODO: ログイン機能を実装後
     # if current_user render home/index else render welcome/index にしたい
     render template: 'welcome/index'
   end
