6988630908730fad35e84ae2cdca0779f311c261
peno022 <40317050+peno022@users.noreply.github.com>
2023-02-14 08:47:04 +0900

    welcomeページのsystem specを追加

--- spec/system/welcomes_spec.rb
+++ spec/system/welcomes_spec.rb
@@ -2,6 +2,18 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Welcomes' do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.describe 'Welcome pages', type: :system do
+  scenario 'user views the welcome pages' do
+    visit root_path
+    expect(page).to have_selector 'nav', text: 'KPIツリーメーカー'
+    expect(page).to have_selector 'button', text: 'ログインなしで試す'
+
+    click_link '利用規約'
+    expect(page).to have_selector 'h1', text: '利用規約'
+
+    click_link '戻る'
+    expect(page).to have_selector 'button', text: 'ログインなしで試す'
+    click_link 'プライバシーポリシー'
+    expect(page).to have_selector 'h1', text: 'プライバシーポリシー'
+  end
 end
