4ba62f5e40a2eabeb6113a63e4d8f353d5b7fe6c
peno022 <40317050+peno022@users.noreply.github.com>
2023-02-19 13:56:13 +0900

    rubocop

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -8,7 +8,7 @@ RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :selenium, using: :headless_chrome, options: {
       browser: :remote,
-      url: ENV.fetch('SELENIUM_DRIVER_URL'),
+      url: ENV.fetch('SELENIUM_DRIVER_URL')
     }
     Capybara.server_host = 'app'
   end
