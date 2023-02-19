4333dfda952851466a3053db787314aa325c58d7
peno022 <40317050+peno022@users.noreply.github.com>
2023-02-20 08:21:16 +0900

    rails g model Tree name:string user:references

---
+++ db/migrate/20230219231724_create_trees.rb
@@ -0,0 +1,10 @@
+class CreateTrees < ActiveRecord::Migration[7.0]
+  def change
+    create_table :trees do |t|
+      t.string :name
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/trees.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :tree do
+    name { "MyString" }
+    user { nil }
+  end
+end
