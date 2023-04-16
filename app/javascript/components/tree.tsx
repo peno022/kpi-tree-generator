debe55addd057da2b5944cc2cfd488ccb9860f64
peno022 <40317050+peno022@users.noreply.github.com>
2023-04-16 14:40:53 +0900

    ノードの階層のハイライトを仮実装（keyを更新する方法でいったん動いた）

--- app/javascript/components/tree.tsx
+++ app/javascript/components/tree.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useSWR from "swr";
 import fetcher from "../fetcher";
 import { convertNodesToRawNodeDatum } from "../convert_nodes_list_to_raw_node_datum";
+import { selectNodes } from "../select_nodes";
 import { createRoot } from "react-dom/client";
 import Tree from "react-d3-tree";
 import * as types from "react-d3-tree/lib/types/types/common";
@@ -9,7 +10,22 @@ import * as typesOfTree from "react-d3-tree/lib/types/Tree/types";
 import CustomNode from "./custom_node";
 
 const EditTree = () => {
+  const initialData: types.RawNodeDatum = {
+    name: "initial-root",
+  };
+
   const { data, error } = useSWR(`/api/trees/1.json`, fetcher);
+  const [nodeDatum, setNodeDatam] = useState<types.RawNodeDatum>(initialData);
+
+  const [treeKey, setTreeKey] = useState<number>(0);
+
+  useEffect(() => {
+    if (data) {
+      const convertedData = convertNodesToRawNodeDatum(data.nodes, data.layers);
+      setNodeDatam(convertedData);
+    }
+  }, [data]);
+
   if (error) return <>エラーが発生しました。</>;
   if (!data) return <>ロード中…</>;
   console.log(data);
@@ -17,15 +33,29 @@ const EditTree = () => {
   console.log(processedData);
 
   const handleClick: typesOfTree.TreeNodeEventCallback = (node) => {
-    console.log("--- clicked ---");
-    console.log(node.data.name);
-    console.log("--- end ---");
+    console.log("--- handleClick start ---");
+    console.log(`id:${node.data?.attributes?.id}: ${node.data.name}をクリック`);
+    // クリックされたノードと同じ親ノードを持つノードのisSelectedをtrueにする
+    if (typeof node.data.attributes?.id === "number") {
+      const afterSelectedNodes = selectNodes(
+        node.data.attributes.id,
+        nodeDatum
+      );
+      setNodeDatam(afterSelectedNodes);
+      // キーを更新してツリーを再描画
+      setTreeKey(treeKey + 1);
+    } else {
+      console.log("idがない又はnumber型でないため処理を中断");
+    }
+    console.dir(nodeDatum, { depth: null });
+    console.log("--- handleClick end ---");
   };
   return (
     <>
       <div id="treeWrapper" style={{ width: "50em", height: "50em" }}>
         <Tree
-          data={processedData}
+          key={treeKey}
+          data={nodeDatum}
           pathFunc="diagonal"
           orientation="vertical"
           renderCustomNodeElement={CustomNode}
