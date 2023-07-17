490488759b6906c50ef5eb1f070b5b6ba0c72acc
peno022 <40317050+peno022@users.noreply.github.com>
2023-07-17 21:46:46 +0900

    SyntaxError: Unexpected token 'export' のエラー解消のためbabelをいれる（WIP）

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,16 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "ts-jest",
+  // preset: "ts-jest",
+  preset: "ts-jest/presets/default-esm",
   testEnvironment: "jsdom",
   modulePathIgnorePatterns: ["<rootDir>/vendor/"],
+  transform: {
+    "^.+\\.[tj]sx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+  transformIgnorePatterns: ["/node_modules/(?!(d3-selection)).+\\.js$"],
 };
