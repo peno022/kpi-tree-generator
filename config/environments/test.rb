1aaeb1afc544bf8ca93b3510821e5db925283d5f
peno022 <40317050+peno022@users.noreply.github.com>
2023-08-01 23:27:54 +0900

    デフォルトのlocaleをjaに設定

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -59,4 +59,5 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+  config.i18n.default_locale = :ja
 end
