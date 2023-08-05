9f2ba3f71f8a8cc43b7e34938b2fdbc0982596dc
peno022 <40317050+peno022@users.noreply.github.com>
2023-08-05 16:27:06 +0900

    GoogleログインCIエラー確認（WIP）

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if Rails.env.test?
-    provider :google_oauth2,
-             ENV.fetch('GOOGLE_CLIENT_ID', nil),
-             ENV.fetch('GOOGLE_CLIENT_SECRET', nil)
-  else
-    provider :google_oauth2,
-             Rails.application.credentials.google[:client_id],
-             Rails.application.credentials.google[:client_secret]
-  end
+  # if Rails.env.test?
+  #   provider :google_oauth2,
+  #            ENV.fetch('GOOGLE_CLIENT_ID', nil),
+  #            ENV.fetch('GOOGLE_CLIENT_SECRET', nil)
+  # else
+  #   provider :google_oauth2,
+  #            Rails.application.credentials.google[:client_id],
+  #            Rails.application.credentials.google[:client_secret]
+  # end
+  provider :google_oauth2,
+           Rails.application.credentials.google[:client_id],
+           Rails.application.credentials.google[:client_secret]
 end
