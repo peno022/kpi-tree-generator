b691e147cda44893545794fed9721e9ab7fdfa9c
peno022 <40317050+peno022@users.noreply.github.com>
2023-08-06 18:20:36 +0900

    CIエラー確認（WIP）

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,18 +4,14 @@ module SessionsHelper
   def current_user
     return unless (user_id = session[:user_id])
 
-    @current_user ||= User.find_by(id: user_id)
+    User.find_by(id: user_id)
   end
 
   def log_in(user)
     session[:user_id] = user.id
   end
 
-  # rubocop:disable Rails/HelperInstanceVariable
-
   def log_out
     session.delete(:user_id)
-    @current_user = nil
   end
-  # rubocop:enable Rails/HelperInstanceVariable
 end
